86eae43ed85c8fa2a725bc8cccbabd49a3f709a3
leeduckgo <albertschr@163.com>
2020-02-03 08:10:55 +0000

    update

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ from app import app
 # app.run(host="0.0.0.0")
 
 if __name__ == '__main__':
-	# app.run(host="0.0.0.0", port=1234, debug=True)
-	app.run(host="0.0.0.0", debug=True, ssl_context=(
-        "server/server-cert.pem",
-        "server/server-key.pem")
-    )
+	app.run(host="localhost", port=1234, debug=True)
+	#app.run(host="0.0.0.0", debug=True, ssl_context=(
+        #"server/server-cert.pem",
+        #"server/server-key.pem")
+    #)
 
